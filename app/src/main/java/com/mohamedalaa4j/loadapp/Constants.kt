f4e7a410cfb4ac5ed02e1faa5f67d7d4e03f557b
mohamedalaa4j <mohamedalaa4j@gmail.com>
2023-01-20 20:01:03 +0200

    successful file download

---
+++ app/src/main/java/com/mohamedalaa4j/loadapp/Constants.kt
@@ -0,0 +1,5 @@
+package com.mohamedalaa4j.loadapp
+
+const val ND940U_URL = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/refs/heads/master.zip"
+const val RETROFIT_URL = "https://github.com/square/retrofit/archive/refs/heads/master.zip"
+const val GLIDE_URL = "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
--- app/src/main/java/com/mohamedalaa4j/loadapp/MainActivity.kt
+++ app/src/main/java/com/mohamedalaa4j/loadapp/MainActivity.kt
@@ -1,15 +1,51 @@
 package com.mohamedalaa4j.loadapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.mohamedalaa4j.loadapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding :ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    private var downloadID: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.tvDownload.setOnClickListener { download() }
+
+    }
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+
+            if (id == downloadID) {
+                Toast.makeText(context, "Download Completed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun download() {
+        val request = DownloadManager.Request(Uri.parse(RETROFIT_URL))
+            .setTitle(getString(R.string.app_name))
+            .setDescription(getString(R.string.app_description))
+            .setRequiresCharging(false)
+            .setAllowedOverMetered(true)
+            .setAllowedOverRoaming(true)
+
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        downloadID = downloadManager.enqueue(request)
+
+        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
     }
 }
